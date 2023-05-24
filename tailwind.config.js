84453c7516f03ac7740afaf93e9359ee099c7ee6
Andrea Leardini <andre8244@gmail.com>
2023-05-24 12:14:40 +0200

    Add color palette; fix NeButton style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,33 @@
+// tailwind cyan palette
+const cyanPalette = {
+  50: '#ecfeff',
+  100: '#cffafe',
+  200: '#a5f3fc',
+  300: '#67e8f9',
+  400: '#22d3ee',
+  500: '#06b6d4',
+  600: '#0891b2',
+  700: '#0e7490',
+  800: '#155e75',
+  900: '#164e63',
+  950: '#083344'
+}
+
+// tailwind purple palette
+const purplePalette = {
+  50: '#faf5ff',
+  100: '#f3e8ff',
+  200: '#e9d5ff',
+  300: '#d8b4fe',
+  400: '#c084fc',
+  500: '#a855f7',
+  600: '#9333ea',
+  700: '#7e22ce',
+  800: '#6b21a8',
+  900: '#581c87',
+  950: '#3b0764'
+}
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
@@ -6,20 +36,7 @@ export default {
       colors: {
         transparent: 'transparent',
         current: 'currentColor',
-        // tailwind cyan palette
-        primary: {
-          50: '#ecfeff',
-          100: '#cffafe',
-          200: '#a5f3fc',
-          300: '#67e8f9',
-          400: '#22d3ee',
-          500: '#06b6d4',
-          600: '#0891b2',
-          700: '#0e7490',
-          800: '#155e75',
-          900: '#164e63',
-          950: '#083344'
-        }
+        primary: purplePalette
       }
     }
   },
